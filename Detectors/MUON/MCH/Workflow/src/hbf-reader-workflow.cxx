d7bb22158306b90986994f611a41ae3904a8f25f
alice-mch <andrea.ferrero@cern.ch>
2020-05-01 11:40:07 +0200

    [MCH] improved RDH handling

--- Detectors/MUON/MCH/Workflow/src/hbf-reader-workflow.cxx
+++ Detectors/MUON/MCH/Workflow/src/hbf-reader-workflow.cxx
@@ -39,28 +39,6 @@
 #include "DPLUtils/DPLRawParser.h"
 #include "Headers/RAWDataHeader.h"
 
-using namespace o2;
-using namespace o2::framework;
-
-struct CRUheader {
-  uint8_t header_version;
-  uint8_t header_size;
-  uint16_t block_length;
-  uint16_t fee_id;
-  uint8_t priority_bit;
-  uint8_t reserved_1;
-  uint16_t next_packet_offset;
-  uint16_t memory_size;
-  uint8_t link_id;
-  uint8_t packet_counter;
-  uint16_t cru_id : 12;
-  uint8_t dpw_id : 4;
-  uint32_t hb_orbit;
-  //uint16_t cru_id;
-  //uint8_t dummy1;
-  //uint64_t dummy2;
-};
-
 namespace o2::header
 {
 extern std::ostream& operator<<(std::ostream&, const o2::header::RAWDataHeaderV4&);
@@ -73,9 +51,73 @@ namespace mch
 namespace raw
 {
 
+/// RAWDataHeaderV4V5V6
+/// RDH structure with fields common to V4, V5 and V6
+/// The common fields are enough to determine the size of the payload
+/// and the offset of the next HB frame
+/// Description of the fields can be found here
+/// https://gitlab.cern.ch/AliceO2Group/wp6-doc/-/blob/master/rdh/RDHv6.md
+//
+///
+///       63     56      48      40      32      24      16       8       0
+///       |---------------|---------------|---------------|---------------|
+///
+///       | reserved              | priori|               |    header     |
+/// 0     | reserve zero  |Source | ty bit|    FEE id     | size  |version|
+///
+/// 1     |ep | cru id    |pcount|link id |  memory size  |offset nxt pack|
+///
+struct RAWDataHeaderV4V5V6 {
+  union {
+    // default value
+    uint64_t word0 = 0x00000000ffff4006;
+    //                       | |     | version 6
+    //                       | |   | 8x64 bit words = 64 (0x40) byte
+    //                       | | invalid FEE id
+    //                       | priority bit 0
+    struct {
+      uint64_t version : 8;       /// bit  0 to  7: header version
+      uint64_t headerSize : 8;    /// bit  8 to 15: header size
+      uint64_t feeId : 16;        /// bit 16 to 31: FEE identifier
+      uint64_t priority : 8;      /// bit 32 to 39: priority bit
+      uint64_t sourceID : 8;      /// bit 40 to 47: source ID
+      uint64_t zero0 : 16;        /// bit 48 to 63: zeroed
+    };                            ///
+  };                              ///
+  union {                         ///
+    uint64_t word1 = 0x0;         /// data written by the CRU
+    struct {                      ///
+      uint32_t offsetToNext : 16; /// bit 64 to 79:  offset to next packet in memory
+      uint32_t memorySize : 16;   /// bit 80 to 95:  memory size
+      uint32_t linkID : 8;        /// bit 96 to 103: link id
+      uint32_t packetCounter : 8; /// bit 104 to 111: packet counter
+      uint16_t cruID : 12;        /// bit 112 to 123: CRU ID
+      uint32_t endPointID : 4;    /// bit 124 to 127: DATAPATH WRAPPER ID: number used to
+    };                            ///                 identify one of the 2 End Points [0/1]
+  };                              ///
+  union {                         ///
+    uint64_t word2 = 0x0;         ///
+  };                              ///
+  union {                         ///
+    uint64_t word3 = 0x0;         /// bit  0 to 63: zeroed
+  };                              ///
+  union {                         ///
+    uint64_t word4 = 0x0;         ///
+  };                              ///
+  union {                         ///
+    uint64_t word5 = 0x0;         /// bit  0 to 63: zeroed
+  };                              ///
+  union {                         ///
+    uint64_t word6 = 0x0;         ///
+  };                              ///
+  union {                         ///
+    uint64_t word7 = 0x0;         /// bit  0 to 63: zeroed
+  };
+};
+
 using namespace o2;
 using namespace o2::framework;
-using RDHv4 = o2::header::RAWDataHeaderV4;
+using RDH = RAWDataHeaderV4V5V6;
 
 class FileReaderTask
 {
@@ -105,39 +147,52 @@ class FileReaderTask
   //_________________________________________________________________________________________________
   void run(framework::ProcessingContext& pc)
   {
-    uint32_t CRUbuf[4 * 4];
-    CRUheader CRUh;
     /// send one RDH block via DPL
+    RDH rdh;
 
-    int RDH_BLOCK_SIZE = 8192;
+    // size of the HB frame to be sent
+    int frameSize = {0};
 
+    // stop if the required number of frames has been reached
     if (mFrameMax == 0) {
       pc.services().get<ControlService>().endOfStream();
       return;
     }
 
-    if (mPrint)
+    if (mPrint) {
       printf("mFrameMax: %d\n", mFrameMax);
-    if (mFrameMax > 0)
+    }
+    if (mFrameMax > 0) {
       mFrameMax -= 1;
-
-    mInputFile.read((char*)(&CRUbuf), sizeof(CRUbuf));
-    memcpy(&CRUh, CRUbuf, sizeof(CRUheader));
-    if (mPrint) {
-      std::cout << "header_version=" << (int)CRUh.header_version << std::endl;
     }
-    if (CRUh.header_version != 4 || CRUh.header_size != 64)
+
+    // read the next RDH
+    mInputFile.read((char*)(&rdh), sizeof(RDH));
+    if (mPrint) {
+      std::cout << "header_version=" << (int)rdh.version << std::endl;
+    }
+
+    // only RDH versions from 4 to 6 are supported
+    if (rdh.version < 4 || rdh.version > 6 || rdh.headerSize != 64) {
       return;
-
-    RDH_BLOCK_SIZE = CRUh.next_packet_offset;
-    if (mPrint) {
-      std::cout << "RDH_BLOCK_SIZE=" << RDH_BLOCK_SIZE << std::endl;
     }
 
-    char* buf = (char*)malloc(RDH_BLOCK_SIZE);
-    memcpy(buf, CRUbuf, CRUh.header_size);
+    // get the frame size from the RDH offsetToNext field
+    frameSize = rdh.offsetToNext;
+    if (mPrint) {
+      std::cout << "frameSize=" << frameSize << std::endl;
+    }
 
-    mInputFile.read(buf + CRUh.header_size, RDH_BLOCK_SIZE - CRUh.header_size);
+    // allocate the output buffer
+    char* buf = (char*)malloc(frameSize);
+
+    // copy the RDH into the output buffer
+    memcpy(buf, &rdh, rdh.headerSize);
+
+    // read the frame payload into the output buffer
+    mInputFile.read(buf + rdh.headerSize, frameSize - rdh.headerSize);
+
+    // stop if data cannot be read completely
     if (mInputFile.fail()) {
       if (mPrint) {
         std::cout << "end of file reached" << std::endl;
@@ -149,18 +204,17 @@ class FileReaderTask
 
     // create the output message
     auto freefct = [](void* data, void* /*hint*/) { free(data); };
-    pc.outputs().adoptChunk(Output{"ROUT", "RAWDATA"}, buf, RDH_BLOCK_SIZE, freefct, nullptr);
+    pc.outputs().adoptChunk(Output{"ROUT", "RAWDATA"}, buf, frameSize, freefct, nullptr);
   }
 
  private:
-  size_t nrdhs{0};
-
   std::ifstream mInputFile{}; ///< input file
   int mFrameMax;              ///< number of frames to process
-  bool mPrint = false;        ///< print digits
+  bool mPrint = false;        ///< print debug messages
 };
 
 //_________________________________________________________________________________________________
+// clang-format off
 o2::framework::DataProcessorSpec getFileReaderSpec()
 {
   return DataProcessorSpec{
@@ -168,10 +222,11 @@ o2::framework::DataProcessorSpec getFileReaderSpec()
     Inputs{},
     Outputs{OutputSpec{"ROUT", "RAWDATA", 0, Lifetime::Timeframe}},
     AlgorithmSpec{adaptFromTask<FileReaderTask>()},
-    Options{{"infile", VariantType::String, "data.raw", {"input file name"}},
-            {"nframes", VariantType::Int, 0, {"number of frames to process"}},
+    Options{{"infile", VariantType::String, "", {"input file name"}},
+            {"nframes", VariantType::Int, -1, {"number of frames to process"}},
             {"print", VariantType::Bool, false, {"verbose output"}}}};
 }
+// clang-format on
 
 } // end namespace raw
 } // end namespace mch
@@ -180,7 +235,6 @@ o2::framework::DataProcessorSpec getFileReaderSpec()
 using namespace o2;
 using namespace o2::framework;
 
-// clang-format off
 WorkflowSpec defineDataProcessing(const ConfigContext&)
 {
   WorkflowSpec specs;
@@ -191,4 +245,3 @@ WorkflowSpec defineDataProcessing(const ConfigContext&)
 
   return specs;
 }
-// clang-format on
